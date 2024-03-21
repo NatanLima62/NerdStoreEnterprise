58d191266b0031130de9b7aea78f7a2e60f3bdb8
NatanLima <natanlimaoliveira62@gmail.com>
2024-03-20 23:29:00 -0300

    iniciando mvc

---
+++ Src/Web/NSE.WebApp.Mvc/Controllers/IdentidadeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.Mvc.Models;
+
+namespace NSE.WebApp.Mvc.Controllers;
+
+public class IdentidadeController : Controller
+{
+    [HttpGet]
+    [Route("nova-conta")]
+    public IActionResult Registro()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    [Route("nova-conta")]
+    public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+    {
+        if (!ModelState.IsValid) return View(usuarioRegistro);
+        
+        //Api - Registro
+
+        if (false) return View(usuarioRegistro);
+        
+        //Realizar login na API
+        
+        return RedirectToAction("Index", "Home");
+    }
+    
+    [HttpGet]
+    [Route("login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+    {
+        // if (!ModelState.IsValid) return View(usuarioLogin);
+        //
+        // //Api - Login
+        //
+        // if (false) return View(usuarioLogin);
+        //
+        // //Realizar login na API
+        
+        return RedirectToAction("Index", "Home");
+    }
+    
+    [HttpGet]
+    [Route("sair")]
+    public async Task<IActionResult> Logout()
+    {
+        //Api - Logout
+
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ Src/Web/NSE.WebApp.Mvc/Models/UsuarioRegistro.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.WebApp.Mvc.Models;
+
+public class UsuarioRegistro
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    public string Email { get; set; } = null!;
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; } = null!;
+    
+    [Display(Name = "Confirme a senha")]
+    [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+    public string SenhaConfirmacao { get; set; } = null!;
+}
+
+public class UsuarioLogin
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    public string Email { get; set; } = null!;
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; } = null!;
+}
